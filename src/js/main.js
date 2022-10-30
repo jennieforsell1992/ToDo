c6aeb662625814ac1659131c558085edba17375e
jennie Forsell <forselljennie@gmail.com>
2022-10-30 17:13:22 +0100

    lagt in li i javascript och gjort en funktion som inte helt funktionell just nu

--- src/js/main.js
+++ src/js/main.js
@@ -8,4 +8,57 @@ class ToDo {
     }
 }
 
-let myToDoList = new ToDo("diska","gå till gymmet","laga mat", "plugga på javascript");
+let toDoList = new ToDo("diska","gå till gymmet","laga mat", "plugga på javascript");
+
+let containerList = document.getElementById("toDoContainer");
+containerList.className = "containerList";
+let checkboxes = document.getElementById("myCheckboxes");
+checkboxes.className = "checkboxesStyle";
+
+let ulList = document.getElementById("toDoList");
+ulList.className = "ulStyle";
+
+let myList = [toDoList];
+
+for (let i = 0; i < myList.length; i++){
+let liListOne = document.createElement("li");
+let liListTwo = document.createElement("li");
+let liListThree = document.createElement("li");
+let liListFour = document.createElement("li");
+
+ulList.appendChild(liListOne);
+ulList.appendChild(liListTwo);
+ulList.appendChild(liListThree);
+ulList.appendChild(liListFour);
+
+liListOne.innerHTML += myList[i].clean;
+liListTwo.innerHTML += myList[i].workout;
+liListThree.innerHTML += myList[i].eat;
+liListFour.innerHTML += myList[i].studying;
+
+let myCheckbox = document.getElementById("myCheckbox")
+myCheckbox.addEventListener("click", checked);
+
+function checked(){
+
+if(myCheckbox.checked === true){
+   liListOne.innerHTML = "Klar!";
+   
+  
+   
+}
+else{
+    return liListOne.innerHTML = myList[i].clean;
+
+}
+}
+
+
+
+}
+
+
+
+
+
+
