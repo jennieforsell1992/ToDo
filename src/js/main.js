09dfca5e6ac4e99a1fbff633f98497632924e12d
jennie Forsell <forselljennie@gmail.com>
2022-10-30 12:42:49 +0100

    Gjort en class i main js och en variabel till min class

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+class ToDo {
+    constructor(clean, workout, eat, studying){
+        this.clean = clean; 
+        this.workout = workout;
+        this.eat = eat;
+        this.studying = studying; 
+       
+    }
+}
+
+let myToDoList = new ToDo("diska","gå till gymmet","laga mat", "plugga på javascript");
